7c69241763c6dd8ce178a0ddb3fe5f04ce911607
ojaszw <alok87055@gmail.com>
2020-04-25 13:37:03 -0700

    Seventh Commit Lab 3 ( Client Part b)

---
+++ RestClient/src/main/java/com/capg/demo/RestClientApplication.java
@@ -0,0 +1,14 @@
+package com.capg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class RestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientApplication.class, args);
+	}
+
+}
